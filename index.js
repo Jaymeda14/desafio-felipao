5c427f8fcfc8c679937e38b45a01cff89a32e13b
Jaymeda14 <cyberda14@gmail.com>
2024-06-08 17:17:10 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,19 +4,19 @@ let xp = 3000
 function write(){
     if (xp <= 1000){
         console.log("O Herói de nome " + heroi + " está no nivel Ferro");
-    }if (xp > 1000 && xp <= 2000){
+    }else if (xp > 1000 && xp <= 2000){
         console.log("O Herói de nome " + heroi + " está no nivel Bronze");
-    }if (xp > 2000 && xp <= 5000){
+    }else if (xp > 2000 && xp <= 5000){
         console.log("O Herói de nome " + heroi + " está no nivel Prata");
-    }if (xp > 5000 && xp <= 7000){
+    }else if (xp > 5000 && xp <= 7000){
         console.log("O Herói de nome " + heroi + " está no nivel Ouro");
-    }if (xp > 7000 && xp <= 8000){
+    }else if (xp > 7000 && xp <= 8000){
         console.log("O Herói de nome " + heroi + " está no nivel Platina");
-    }if (xp > 8000 && xp <= 9000){
+    }else if (xp > 8000 && xp <= 9000){
         console.log("O Herói de nome " + heroi + " está no nivel Ascendente");
-    } if (xp > 9000 && xp <= 10000){
+    }else if (xp > 9000 && xp <= 10000){
         console.log("o Herói de nome " + heroi + " está no nivel Imortal");
-    }if (xp > 10000){
+    }else if (xp > 10000){
         console.log("o Herói de nome " + heroi + " está no nivel Radiante");
     }
 }
